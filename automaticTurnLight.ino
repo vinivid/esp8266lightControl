69e77e6915c758539ae7a41b996fef536d3fc1d0
vini <frato.vini@gmail.com>
2024-07-03 21:05:07 -0300

    win

--- automaticTurnLight.ino
+++ automaticTurnLight.ino
@@ -3,17 +3,66 @@
 #include <ESPAsyncWebServer.h>
 #include <FS.h>
 #include <LittleFS.h>
+#include <ArduinoJson.h>
 
+//Nome e senha da rede para conectar na ESP
 const char* ssid = "controleLuz";
 const char* passWord = "123456789";
 
-int sensorPin = 0;
-
-int presenceState = 0;
 int lightState = 1;
+
 int useSwitch = 1;
+int switchPress = 0;
 
 AsyncWebServer server(80);
+
+//Variaveis relacionadas ao pino sensor e quantidade de vezes que foi utilizado
+int sensorPin = 0;
+int presenceState = 0;
+int countSensorQt = 0;
+int sensorCountState = 0;
+
+//variaveis que contão o tempo que uma lampada ficou ligada
+long int timeLightOn = 0;
+int timeCountStart = 0;
+long int miliP1 = 0;
+long int miliP2 = 0;
+
+/*A função readJSON() é executada uma vez no programa e sua função é apenas ler os dados armazenados
+no JSON e coloca-los em suas devidas variaveis de contagem
+*/
+void readJSON(){
+  File storeFile = LittleFS.open("/store.json", "r");
+  if(!(storeFile)){
+    Serial.println("Falha ao abrir storage"); return;
+  }
+
+  DynamicJsonDocument storage(1024);
+  deserializeJson(storage, storeFile);
+  storeFile.close();
+
+  timeLightOn = storage["time"];
+  countSensorQt = storage["sensor"];
+  switchPress = storage["switches"];
+}
+
+/*A função saveJSON() é usada 
+*/
+void saveJSON(){
+  File storeFile = LittleFS.open("/store.json", "w");
+  if(!(storeFile)){
+    Serial.println("Falha ao abrir storage"); return;
+  }
+
+  DynamicJsonDocument storage(1024);
+
+  storage["time"] = timeLightOn;
+  storage["sensor"] = countSensorQt;
+  storage["switches"] = switchPress;
+
+  serializeJson(storage, storeFile);
+  storeFile.close();
+}
 
 void setup() {
   Serial.begin(9600);
@@ -24,7 +73,6 @@ void setup() {
   IPAddress IP = WiFi.softAPIP();
 
   Serial.println(IP);
-  Serial.println(WiFi.localIP()); 
 
   pinMode(sensorPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -37,49 +85,91 @@ void setup() {
     Serial.println("Erro ao abrir littleFS"); return;
   }
 
-  //Trata todos os pedidos de arquivo ou de estado
+  //lê o que havia sido salvo previamente
+  readJSON();
+
+  //Envia o html
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(LittleFS, "/index.html");
     Serial.println("Sucess html");
   });
 
+  //Envia o css
   server.on("/look.css", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(LittleFS, "/look.css");
     Serial.println("Sucess css");
   });
 
+  //Envia o javascript
   server.on("/control.js", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(LittleFS, "/control.js");
     Serial.println("Sucess js");
   });
 
+  //Antes de enviar para a pagina atualiza o que estava no JSON
+  server.on("/store", HTTP_GET, [](AsyncWebServerRequest *request){
+    saveJSON();
+    request->send(LittleFS, "/store.json");
+    Serial.println("Sucess json");
+  });
+
+  //Envia o texto para a pagina com base no estado da lâmpada
   server.on("/lightState", HTTP_GET, [](AsyncWebServerRequest *request){
     Serial.println("lighstate asked");
     if(lightState == 0) request->send_P(200, "text/plain", "0");
     if(lightState == 1) request->send_P(200, "text/plain", "1");
   });
 
+  //Quando a luz é ligada pelo interruptor remoto
   server.on("/lightON", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", "ok");
     Serial.println("light on asked");
     useSwitch = 0;
     lightState = 0;
+    switchPress += 1;
   });
 
+  //Quando a luz é desligada pelo interruptor remoto
   server.on("/lightOFF", HTTP_GET, [](AsyncWebServerRequest *request){
     Serial.println("light off asked");
     request->send_P(200, "text/plain", "ok");
     useSwitch = 1;
     lightState = 1;
+    switchPress += 1;
   });
 }
 
 void loop() {
+  //Vê se o sensor esta detectando algo
   presenceState = digitalRead(sensorPin);
-  if(useSwitch == 1) lightState = presenceState;
+
+  //Lógica para testar quantas vezes o sensor foi usado
+  if(useSwitch == 1){
+    lightState = presenceState;
+    if(sensorCountState == 0 && lightState == 0){
+      countSensorQt += 1;
+      sensorCountState = 1;
+    }else if(sensorCountState == 1 && lightState == 1){
+      sensorCountState = 0;
+    }
+  }else{
+    sensorCountState = 0;
+  }
+
+  //Contador de tempo em que a luz ficou acessa em mili segundos, também liga ou desliga a luz
   if(lightState == 0){
+    if(timeCountStart == 0){
+      miliP1 = millis();
+      timeCountStart = 1;
+    }
     digitalWrite(LED_BUILTIN, LOW);
   }else{
+    if(timeCountStart == 1){
+      miliP2 = millis();
+      timeLightOn += miliP2-miliP1;
+      Serial.println(timeLightOn);
+      timeCountStart = 0;
+    }
     digitalWrite(LED_BUILTIN, HIGH);
   }
 }
--- data/control.js
+++ data/control.js
@@ -1,20 +1,29 @@
+/*JavaScript simples com o intuito apenas de constantemente
+escanear os dados adquiridos pela esp. 
+*/
 let lightState = -1;
-
 var lightSwitch = document.getElementById('lamp1switch');
 var lampStateHTML = document.getElementById('lamp1State');
 
+//Lê se o botão da pagina foi pressionado para ligar ou desligar a luz
 lightSwitch.addEventListener('change', ()=>{
     if(lightSwitch.checked){
+        //Pede para ligar a luz
         let xhttp = new XMLHttpRequest();
         xhttp.open("GET", "/lightON", true);
         xhttp.send();
         lightState = 0;
+
+        //muda o icone da lâmpada
         lampStateHTML.className = "lampstateON";
     }else{
+        //Pede para desligar a luz
         let xhttp = new XMLHttpRequest();
         xhttp.open("GET", "/lightOFF", true);
         xhttp.send();
         lightState = 1;
+
+        //muda o icone da lâmpada
         lampStateHTML.className = "lampstateOFF";
     }
 });
@@ -24,12 +33,14 @@ setInterval(() => {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
+            //o programa sempre retornara 0 ou 1 como estado da luz
             lightState = xhttp.responseText;
         }
     };
     xhttp.open("GET", "/lightState", true);
     xhttp.send();
 
+    //corrigi o botão e o icone da lampâda se estiverem na posição errada
     if(lightState == 0){
         lightSwitch.checked = true;
         lampStateHTML.className = "lampstateON"
@@ -40,3 +51,32 @@ setInterval(() => {
         lightSwitch.checked = false;
     }
 }, 2000);
+
+//Essa função lê os dados armazenados no JSON pela ESP
+async function readJson(){
+    let savedThings;
+    let pageLig = document.getElementById('luzLig');
+    let pageSens = document.getElementById('ligPSens');
+    let ligSwitch = document.getElementById('LigSwitch');
+
+    try{
+        const response = await fetch('/store');
+        if (!response.ok) {
+            throw new Error(`Stado: ${response.status}`);
+        }
+  
+        const json = await response.json();
+        console.log(json);
+        savedThings = json;
+        pageLig.innerHTML = `Luz ligada por: ${(savedThings.time)/60000} min`;
+        pageSens.innerHTML = `Luz ligada pelo sensor: ${savedThings.sensor} vezes`;
+        ligSwitch.innerHTML = `Usou o interruptor: ${savedThings.switches} vezes`;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+//Como mandar o JSON requer um pouco a mais de tempo o melhor é fazer por ultimo
+readJson()
+
+setInterval(readJson, 15000);
